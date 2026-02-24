92ada8d1ada42de115823b88daefd10ab66dcd77
Karunanidhi Sharma <Karunanidhisharma16@users.noreply.github.com>
2026-02-24 18:20:06 +0530

    feat: update API and socket URLs to use environment variables

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // const SOCKET_URL = 'http://localhost:5000';
-const SOCKET_URL = 'https://chatconnect-2siz.onrender.com';
+const SOCKET_URL = import.meta.env.VITE_API_URL;
 export const socket = io(SOCKET_URL, {
   autoConnect: false
 });
